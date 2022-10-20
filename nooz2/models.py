58d4736eec9b1a912389da1f131bec34ec3c7875
mpysys <monnet.morgan@gmail.com>
2022-10-20 17:44:50 +0000

    Add timestamp field in database and order posts

--- nooz2/models.py
+++ nooz2/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from datetime import datetime, date
 # Create your models here.
 
 
@@ -11,6 +11,7 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    timestamp = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- nooz2/views.py
+++ nooz2/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    ordering = ['-id']
+    ordering = ['-timestamp']
 
 
 class PostDetailView(DetailView):
