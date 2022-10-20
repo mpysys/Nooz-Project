e3cf4f25424b4d2d93157b88eaafc79d1b73d2cd
mpysys <monnet.morgan@gmail.com>
2022-10-20 21:34:45 +0000

    Created subs app for user registration and login

--- nooz/settings.py
+++ nooz/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'nooz2',
+    'subs',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- nooz/urls.py
+++ nooz/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nooz2.urls')),
+    path('subs/', include('django.contrib.auth.urls')),
+    path('subs/', include('subs.urls')),
+
 ]
