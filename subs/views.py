4c1fc53d5a8c6b87830b53fcc098a46f83eaa8cf
mpysys <monnet.morgan@gmail.com>
2022-10-26 03:18:02 +0000

    Add fields and functionality to registration form

--- subs/views.py
+++ subs/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
+from .forms import SignUpForms
 # Create your views here.
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForms
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
