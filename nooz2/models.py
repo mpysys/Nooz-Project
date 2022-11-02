7c30ad500a3d7055a6ee911e384dcb8afc0fd78b
mpysys <monnet.morgan@gmail.com>
2022-11-02 19:02:53 +0000

    Added a blog functionality and section on post details

--- nooz2/models.py
+++ nooz2/models.py
@@ -53,3 +53,13 @@ class Profile(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
